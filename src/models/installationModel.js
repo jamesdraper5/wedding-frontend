97df0910722227a86fac8f65b6d17dd5e3dd8bb9
James Draper <jamesdrape@gmail.com>
2016-08-16 17:48:39 +0100

    Updated models

--- src/models/installationModel.js
+++ src/models/installationModel.js
@@ -3,17 +3,15 @@ import * as mapping from 'knockout-mapping';
 
 class InstallationModel {
     constructor(data) {
-
         mapping.fromJS(data, {}, this);
         this.activeSections = ko.pureComputed(this.generateLinks, this);
-
     }
 
     generateLinks() {
         var activeSections = [];
         var sections = this.sections;
         for ( var section in sections ) {
-            if ( sections.hasOwnProperty(section) && this.sections[section] != null ) {
+            if ( sections.hasOwnProperty(section) && this.sections[section] != null ) { // TO DO: `&& this.sections[section].isVisible` - waiting on adding `isVisible` to each JSON section
                 activeSections.push( this.sections[section].linkText );
             }
         }
--- src/models/mapModel.js
+++ src/models/mapModel.js
@@ -1,14 +1,11 @@
 import ko from 'knockout';
 import * as mapping from 'knockout-mapping';
 
-class InstallationModel {
+class MapModel {
     constructor(data) {
-
         mapping.fromJS(data, {}, this);
-
-
     }
 
 }
 
-export default InstallationModel;
+export default MapModel;
