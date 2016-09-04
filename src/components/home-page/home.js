35dfaffd91f1caa708003ace4c8d344190e0ea28
James Draper <jamesdrape@gmail.com>
2016-09-04 16:32:57 +0100

    File upload - very early WIP

--- src/components/home-page/home.js
+++ src/components/home-page/home.js
@@ -6,8 +6,62 @@ class HomeViewModel {
         this.installation = app.installation;
         this.showAdmin = ko.observable(route.showAdmin || false);
 
-        //console.log('route', route);
+        this.listenToInput();
     }
+
+    OnRendered() {
+    	//ko.postbox.publish('home-page-rendered');
+
+    	if ( app.hasSidebar() ) {
+    		setTimeout(() => {
+	    		app.sidebarPosition('open');
+    		}, 500);
+    	}
+    }
+
+    listenToInput() {
+	    document.getElementById("file-input").onchange = () => {
+	        const files = document.getElementById('file-input').files;
+	        const file = files[0];
+	        if(file == null){
+	          return alert('No file selected.');
+	        }
+	        this.getSignedRequest(file);
+	    }
+    }
+
+    getSignedRequest(file) {
+      	const xhr = new XMLHttpRequest();
+      	xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+      	xhr.onreadystatechange = () => {
+        	if(xhr.readyState === 4){
+          		if(xhr.status === 200){
+            		const response = JSON.parse(xhr.responseText);
+            		this.uploadFile(file, response.signedRequest, response.url);
+          		} else {
+		            alert('Could not get signed URL.');
+	            }
+        	}
+      	};
+      	xhr.send();
+    }
+
+    uploadFile(file, signedRequest, url){
+		const xhr = new XMLHttpRequest();
+		xhr.open('PUT', signedRequest);
+		xhr.onreadystatechange = () => {
+        	if(xhr.readyState === 4){
+          		if(xhr.status === 200){
+            		document.getElementById('preview').src = url;
+            		document.getElementById('avatar-url').value = url;
+          		} else {
+            		alert('Could not upload file.');
+          		}
+        	}
+      	};
+	    xhr.send(file);
+    }
+
 }
 
 export default { viewModel: HomeViewModel, template: homeTemplate };
