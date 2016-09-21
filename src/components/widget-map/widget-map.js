0c800849fc4c2014943c4a58a496e496cc8f9369
James Draper <jamesdrape@gmail.com>
2016-09-21 22:12:33 +0100

    Added address search functionality to map in edit overlay

--- src/components/widget-map/widget-map.js
+++ src/components/widget-map/widget-map.js
@@ -3,106 +3,123 @@ import templateMarkup from 'text!./widget-map.html';
 import * as GoogleMapsLoader from 'google-maps';
 
 class WidgetMap {
-    constructor(params) {
-    	GoogleMapsLoader.KEY = 'AIzaSyBdlmh7fqtInzVycyxMmzS4n9rAvZSlZYI';
+	constructor(params) {
+		GoogleMapsLoader.KEY = 'AIzaSyBdlmh7fqtInzVycyxMmzS4n9rAvZSlZYI';
 
-    	// TO DO: Add map marker to styles
-    	this.map = params.map;
-    	this.uid = Date.now();
-    	this.mapHeight = params.mapHeight || '300px';
-    	this.isEditMode = params.isEditMode || false;
-    	this.googleMap = null;
-    	this.googleMarker = null;
-    	this.themeStyles = {
-    		paris: [
-    			{
-    				featureType: "all",
-    				stylers: [{ saturation: -80 }]
-    			},
-    			{
-    				featureType: "road.arterial",
-    				elementType: "geometry",
-    				stylers: [{ hue: "#00ffee" }, { saturation: 50 }]
-    			},
-    			{
-    				featureType: "poi.business",
-    				elementType: "labels",
-    				stylers: [{ visibility: "off" }]
-    			}
-    		]
-    	};
-    	this.subscriptions = [];
-    	this.setInitialLocation();
+		// TO DO: Add map marker to styles
+		this.map = params.map;
+		this.uid = Date.now();
+		this.mapHeight = params.mapHeight || '300px';
+		this.isEditMode = params.isEditMode || false;
+		this.googleMap = null;
+		this.googleMarker = null;
+		this.google = null;
+		this.themeStyles = {
+			paris: [
+				{
+					featureType: "all",
+					stylers: [{ saturation: -80 }]
+				},
+				{
+					featureType: "road.arterial",
+					elementType: "geometry",
+					stylers: [{ hue: "#00ffee" }, { saturation: 50 }]
+				},
+				{
+					featureType: "poi.business",
+					elementType: "labels",
+					stylers: [{ visibility: "off" }]
+				}
+			]
+		};
+		this.subscriptions = [];
+		this.setInitialLocation();
 
-    }
+		if ( this.isEditMode ) {
+			this.subscriptions.push( ko.postbox.subscribe('update-gmap-location', (data) => this.updateLocation(data) ) )
+		}
+	}
 
-    OnRendered() {
+	OnRendered() {
 		GoogleMapsLoader.load((google) => {
+			this.geocoder = new google.maps.Geocoder();
 			this.renderGoogleMap(google);
 		});
 	}
 
-    renderGoogleMap(google) {
-    	var el = document.getElementById('map' + this.uid);
-    	var locationCenter = {
-    		lat: parseFloat(this.map.latitude(), 10),
-    		lng: parseFloat(this.map.longitude(), 10)
-    	};
-    	var options = {
-    		center: locationCenter,
-    		styles: this.themeStyles[app.installation.themeClass()],
-    		zoom: 15,
-    		scrollwheel: false
-    	};
-    	var pinImage = 'images/heart.png';
+	renderGoogleMap(google) {
+		var el = document.getElementById('map' + this.uid);
+		var locationCenter = {
+			lat: parseFloat(this.map.latitude(), 10),
+			lng: parseFloat(this.map.longitude(), 10)
+		};
+		var options = {
+			center: locationCenter,
+			styles: this.themeStyles[app.installation.themeClass()],
+			zoom: 15,
+			scrollwheel: false
+		};
+		var pinImage = 'images/heart.png';
 
-    	this.googleMap = new google.maps.Map(el, options);
-    	this.googleMarker = new google.maps.Marker({
-    		position: locationCenter,
-    		title: this.map.title(),
-    		animation: google.maps.Animation.DROP,
-    		icon: pinImage,
-    		draggable: this.isEditMode
-    	});
+		this.googleMap = new google.maps.Map(el, options);
+		this.googleMarker = new google.maps.Marker({
+			position: locationCenter,
+			title: this.map.title(),
+			animation: google.maps.Animation.DROP,
+			icon: pinImage,
+			draggable: this.isEditMode
+		});
 
-    	if ( this.isEditMode ) {
-    		console.log('doing edit stuff');
-	    	google.maps.event.addListener(this.googleMarker, 'dragend', () => {
-	    		this.geocodePosition(this.googleMarker.getPosition());
-	    	});
+		if ( this.isEditMode ) {
+			google.maps.event.addListener(this.googleMarker, 'dragend', () => {
+				this.geocodePosition(this.googleMarker.getPosition());
+			});
 
-	    	this.subscriptions.push( this.map.title.subscribe((title) => {
-	    		this.googleMarker.setTitle(title)
-	    	}));
-    	}
+			this.subscriptions.push( this.map.address.subscribe((address) => {
+				this.googleMarker.setTitle(address);
+			}));
+		}
 
-    	// To add the marker to the map, call setMap();
-    	this.googleMarker.setMap(this.googleMap)
-    }
+		// To add the marker to the map, call setMap();
+		this.googleMarker.setMap(this.googleMap)
+	}
 
+	// @pos: { lat: float, lng: float }
 	geocodePosition(pos) {
-	   var geocoder = new google.maps.Geocoder();
-	   geocoder.geocode({
-	        latLng: pos
-	    }, (results, status) => {
-            if (status == google.maps.GeocoderStatus.OK) {
-                this.map.latitude( pos.lat() )
-                this.map.latitude( pos.lng() )
-                this.map.title( results[0].formatted_address )
+	   	this.geocoder.geocode({
+			latLng: pos
+		}, (results, status) => {
+			if (status == google.maps.GeocoderStatus.OK) {
+				this.map.latitude( pos.lat() )
+				this.map.latitude( pos.lng() )
+				this.map.address( results[0].formatted_address )
 
-            } else {
-            	// Not sure if we need an error message for this, could probably just fail silently...
-                //app.flash.Error( "<strong>Sorry!</strong> ", 'Cannot determine address at this location.' );
-            }
-	    });
+			} else {
+				// Not sure if we need an error message for this, could probably just fail silently...
+				//app.flash.Error( "<strong>Sorry!</strong> ", 'Cannot determine address at this location.' );
+			}
+		});
+	}
+
+	// @address: string
+	geocodeAddress(address) {
+        this.geocoder.geocode({
+        	'address': address
+        }, (results, status) => {
+	        if (status === 'OK' && results.length) {
+	            this.googleMap.setCenter(results[0].geometry.location);
+	            this.googleMarker.setPosition(results[0].geometry.location);
+	            this.map.latitude( results[0].geometry.location.lat() )
+	            this.map.longitude( results[0].geometry.location.lng() )
+	        } else {
+	            alert('Geocode was not successful for the following reason: ' + status);
+	        }
+        });
 	}
 
 	setInitialLocation() {
-		console.log('this', this);
-		console.log('this.isEditMode', this.isEditMode);
 		if ( !this.isEditMode ) return;
 
-		console.log('doing edit stuff 2');
 		// The inital location is set to Times Square in the DB.
 		// If it hasn't been edited then ask user for their current location to set map marker, else return
 		if ( this.map.latitude() !== '40.7505' && this.map.longitude() !== '-73.9934' ) {
@@ -111,6 +128,8 @@ class WidgetMap {
 		if ("geolocation" in navigator) {
 			navigator.geolocation.getCurrentPosition((position) => {
 				this.moveMarker( position.coords.latitude, position.coords.longitude )
+				this.map.latitude( position.coords.latitude );
+				this.map.latitude( position.coords.longitude );
 			});
 		}
 	}
@@ -120,12 +139,18 @@ class WidgetMap {
 		this.googleMarker.setPosition( new google.maps.LatLng( lat, lng ) );
 		this.map.latitude( lat );
 		this.map.latitude( lng );
-	};
+	}
+
+	updateLocation(data) {
+		if ( data.id === this.map.id() ) {
+			this.geocodeAddress(data.address);
+		}
+	}
 
 	dispose() {
-	    this.subscriptions.forEach((sub) => {
-	        sub.dispose()
-	    })
+		this.subscriptions.forEach((sub) => {
+			sub.dispose()
+		})
 	}
 }
 
--- src/models/mapModel.js
+++ src/models/mapModel.js
@@ -4,8 +4,14 @@ import * as mapping from 'knockout-mapping';
 class MapModel {
     constructor(data) {
         mapping.fromJS(data, {}, this);
+
+        this.address = ko.observable('') // used for geocoding address string to map co-ords
     }
 
+    updateGoogleMapLocation() {
+    	ko.postbox.publish('update-gmap-location', { id: this.id(), address: this.address() });
+
+    }
 }
 
 export default MapModel;
