0051d186d94426ce3d6ae8dd77d8243e318304cd
James Draper <jamesdrape@gmail.com>
2016-11-05 15:45:03 +0000

    Remove old login code from header

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -33,34 +33,6 @@ class NavBarViewModel {
     	}
 
     }
-
-    OnSubmitLogin() {
-    	var data = {
-    		email: this.userEmail(),
-    		password: this.userPassword()
-    	}
-    	this.isLoginSubmitting(true);
-
-    	app.api.post('api/authenticate', data).then((result) => {
-    		console.log('result', result);
-    		app.getLoggedInUser().then((result) => {
-	    		app.flash.Success(`Welcome back ${app.loggedInUser.firstName()}!`);
-	    		app.sidebarPosition('open');
-    		})
-    	}).catch((result) => {
-        	if ( result.response.message ) {
-	            app.flash.Error( "<strong>Sorry!</strong> ", result.response.message );
-	        }
-    	}).finally(() => {
-    		this.userPassword('');
-    		this.isLoginSubmitting(false);
-    	});
-    }
-
-    OnLogOut() {
-    	app.Logout();
-    }
-
 }
 
 export default { viewModel: NavBarViewModel, template: template };
