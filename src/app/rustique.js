dc0b0610fc66e2d80c9cda5f63b9a7dc61fe95b3
James Draper <jamesdrape@gmail.com>
2016-11-05 15:52:22 +0000

    Show error msg when redirecting to login page

--- src/app/rustique.js
+++ src/app/rustique.js
@@ -243,6 +243,7 @@ class Rustique {
 	redirectToLogin( callback ) {
 	    // Redirect to login but remember this page
 	    if ( app.currentRoute().page !== "login" ) {
+	        this.flash.Error('You shall not pass!', 'You need to be logged in before you can access that page')
 	        app.requestedRouteBeforeLoginRedirect = app.currentRoute()
 	        app.GoTo("#login")
 	    }
