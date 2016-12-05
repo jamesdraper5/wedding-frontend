7f3415181f4f308f295d8145115f25777b095ae0
James Draper <jamesdrape@gmail.com>
2016-12-05 15:20:28 +0000

    Remove log

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.use(express.static('public'));
 app.use(express.static('dist'));
 // not found in static files, so default to index.html
 app.use(function (req, res) {
-	console.log('__dirname', __dirname);
-	res.sendFile(__dirname + '/index.html');
+	return res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(app.get('port'), function () {
