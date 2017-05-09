a8222af327cc852256c51f86863ac27bdef9e89a
James Draper <jamesdrape@gmail.com>
2017-05-09 21:16:28 +0100

    Fix for hardcoded initials in home section

--- src/components/section-home/section-home.js
+++ src/components/section-home/section-home.js
@@ -4,11 +4,18 @@ import templateMarkup from 'text!./section-home.html';
 
 class HomeSection {
 	constructor(params) {
+		this.subscriptions = [];
 		this.homeData = app.installation.sections.home;
 		this.weddingDate = this.homeData.weddingDate.format(app.constants.DATEFORMATS.long);
 		this.containerId = this.homeData.menuText().toLowerCase().split(' ').join('-') + '-container';
 		this.partnerOneName = this.homeData.partnerOneName;
 		this.partnerTwoName = this.homeData.partnerTwoName;
+		this.subscriptions.push(this.partnerOneInitial = ko.pureComputed(() => {
+			return this.partnerOneName().charAt(0);
+		}));
+		this.subscriptions.push(this.partnerTwoInitial = ko.pureComputed(() => {
+			return this.partnerTwoName().charAt(0);
+		}));
 	}
 
 	dispose() {
