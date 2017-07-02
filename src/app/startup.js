07466b5f24f17df6c4b785a1a31c8f077908e029
James Draper <jamesdrape@gmail.com>
2017-07-02 20:05:32 +0100

    Move footer CTA to component

--- src/app/startup.js
+++ src/app/startup.js
@@ -34,7 +34,7 @@ import widgetImageEditor from 'components/widget-image-editor/widget-image-edito
 import modalImagePicker from 'components/modal-image-picker/modal-image-picker';
 import modalUploadImage from 'components/modal-upload-image/modal-upload-image';
 import modalChangePassword from 'components/modal-change-password/modal-change-password';
-
+import widgetFooterCta from 'components/widget-footer-cta/widget-footer-cta';
 
 // Components can be packaged as AMD modules, such as the following:
 ko.components.register('nav-bar', { viewModel: navBar.viewModel, template: navBar.template });
@@ -67,6 +67,7 @@ ko.components.register('widget-image-editor', { viewModel: widgetImageEditor.vie
 ko.components.register('modal-image-picker', { viewModel: modalImagePicker.viewModel, template: modalImagePicker.template });
 ko.components.register('modal-upload-image', { viewModel: modalUploadImage.viewModel, template: modalUploadImage.template });
 ko.components.register('modal-change-password', { viewModel: modalChangePassword.viewModel, template: modalChangePassword.template });
+ko.components.register('widget-footer-cta', { viewModel: widgetFooterCta.viewModel, template: widgetFooterCta.template }); 
 // [Scaffolded component registrations will be inserted here. To retain this feature, don't remove this comment.]
 
 window.app = new app();
---
+++ src/components/widget-footer-cta/widget-footer-cta.js
@@ -0,0 +1,25 @@
+import ko from 'knockout';
+import templateMarkup from 'text!./widget-footer-cta.html';
+
+class WidgetFooterCta {
+    constructor(params) {
+        this.isTokenLoaded = ko.observable(false);
+        this.getPaymentToken();
+        this.token = ko.observable(null);
+    }
+
+    getPaymentToken() {
+        app.api.get('/api/authenticate/getpaymenttoken').then((result) => {
+            console.log('result', result)
+            this.token(result.response.data.token);
+            this.isTokenLoaded(true);
+        });
+    }
+
+    dispose() {
+        // This runs when the component is torn down. Put here any logic necessary to clean up,
+        // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
+    }
+}
+
+export default { viewModel: WidgetFooterCta, template: templateMarkup };
